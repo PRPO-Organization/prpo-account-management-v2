6127b6501452cfe0cea6bc5ff06b257de4409cba
StefanJakjoski <stefan.jakjoski@gmail.com>
2026-01-13 15:42:07 +0100

    added tests

--- src/test/java/com/skupina1/accountmanagement/AppTest.java
+++ src/test/java/com/skupina1/accountmanagement/AppTest.java
@@ -1,1 +1,19 @@
 package com.skupina1.accountmanagement;
+
+import com.skupina1.accountmanagement.service.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void userService_canBeCreated() {
+        UserService newService = new UserService();
+        assertNotNull(newService);
+    }
+
+}
